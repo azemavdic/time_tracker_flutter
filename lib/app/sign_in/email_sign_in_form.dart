83ef298eb7af0c38ff3fc153bb83583ee33fd9a1
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-06-01 22:11:00 +0200

    toggle sign in and register

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/common_widgets/form_submit_button.dart';
 
+enum EmailSignInFormType {
+  signIn,
+  register,
+}
+
 class EmailSignInForm extends StatefulWidget {
   @override
   _EmailSignInFormState createState() => _EmailSignInFormState();
@@ -8,16 +13,52 @@ class EmailSignInForm extends StatefulWidget {
 
 class _EmailSignInFormState extends State<EmailSignInForm> {
   bool _passwordVisible;
+  FocusNode myFocusNode;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit() {
+    print(_emailController.text);
+    print(_passwordController.text);
+  }
 
   @override
   void initState() {
     super.initState();
     _passwordVisible = false;
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    myFocusNode.dispose();
+  }
+
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+
+      _emailController.text = '';
+      _passwordController.text = '';
+    });
   }
 
   List<Widget> _buildChildren() {
+    final primaryText =
+        _formType == EmailSignInFormType.signIn ? 'Prijava' : 'Registracija';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Niste registrovani? Registrujte se ovdje'
+        : 'Imate račun? Prijavite se ovdje.';
     return [
       TextField(
+        focusNode: myFocusNode,
+        controller: _emailController,
         decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
           labelText: 'Email',
@@ -26,6 +67,7 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       ),
       SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
         obscureText: !_passwordVisible,
         decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
@@ -44,13 +86,16 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        onPressed: () {},
-        text: 'Prijava',
+        onPressed: _submit,
+        text: primaryText,
       ),
       SizedBox(height: 8.0),
       TextButton(
-        onPressed: () {},
-        child: Text('Niste registrovani? Registrujte se ovdje.'),
+        onPressed: () {
+          _toggleFormType();
+          myFocusNode.requestFocus();
+        },
+        child: Text(secondaryText),
       )
     ];
   }
