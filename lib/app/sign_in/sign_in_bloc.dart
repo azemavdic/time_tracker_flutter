7b0bdf8cdc536d97ee2c92abf7b669cba26678e2
unknown <asko2212@hotmail.com>
2021-06-04 11:27:40 +0200

    submit function fixed

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker/services/auth.dart';
+
 class SignInBloc {
+  SignInBloc({@required this.auth});
+  final AuthBase auth;
   // Controller
   final StreamController<bool> _isLoadingController = StreamController<bool>();
 
@@ -13,5 +19,23 @@ class SignInBloc {
   }
 
   //3.Add value to stream
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  //Sign in methods
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      _setIsLoading(false);
+      rethrow;
+    }
+  }
+
+  Future<User> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+  Future<User> signInWithFacebook() async =>
+      await _signIn(auth.signInWithFacebook);
 }
